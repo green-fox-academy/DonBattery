7a7d0e0fe5723b567edf859fb17eb186be71a075
DonBattery <mikloslorinczi.it@gmail.com>
2017-11-27 15:41:52 +0100

    completed draw_square.py

--- week-02/day-1/Test.py
+++ week-02/day-1/Test.py
@@ -1,3 +1,29 @@
-print ("11 % 5 "+str(11 % 5))
+import os
 
-print ("11 // 5 "+str(11 // 5))
+l = input("Side length (1-35): ")
+l = int(l)
+borderchar = "@"
+
+os.system("clear")
+
+if (l < 1) or (l > 35):
+    print("Length out of range!")
+elif l == 1:
+    print(borderchar)
+else:
+    for y in range(l):
+        if (y == 0) or (y == l-1):
+            print(borderchar * l)
+        else:
+            print((borderchar) + (" " * (l-2)) + (borderchar))
+
+
+
+
+''' a = "O"
+
+x = "." * 10
+
+a = a + x + "O"
+
+print(a) '''
--- week-02/day-1/draw_square.py
+++ week-02/day-1/draw_square.py
@@ -10,3 +10,23 @@
 # %%%%%
 #
 # The square should have as many lines as the number was
+
+import os
+
+print("I will draw a square.")
+l = input("Enter the side length (1-35): ")
+l = int(l)
+borderchar = "%"
+
+os.system("clear")
+
+if (l < 1) or (l > 35):
+    print("Length out of range!")
+elif l == 1:
+    print(borderchar)
+else:
+    for y in range(l):
+        if (y == 0) or (y == l-1):
+            print(borderchar * l)
+        else:
+            print((borderchar) + (" " * (l-2)) + (borderchar))
