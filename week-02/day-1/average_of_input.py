5b60b21fa704c2fd65e9f7af077bb42a75a10cf6
DonBattery <mikloslorinczi.it@gmail.com>
2017-11-27 12:56:45 +0100

    completed average_of_input.py

--- week-02/day-1/average_of_input.py
+++ week-02/day-1/average_of_input.py
@@ -2,3 +2,21 @@
 # then it should print the sum and the average of these numbers like:
 #
 # Sum: 22, Average: 4.4
+
+a1 = input("Please enter the value of A1 ")
+a2 = input("Please enter the value of A2 ")
+a3 = input("Please enter the value of A3 ")
+a4 = input("Please enter the value of A4 ")
+a5 = input("Please enter the value of A5 ")
+
+a1 = int(a1)
+a2 = int(a2)
+a3 = int(a3)
+a4 = int(a4)
+a5 = int(a5)
+
+sum = a1 + a2 + a3 + a4 + a5
+
+avarage = sum / 5
+
+print("The Sum of As is "+str(sum)+" And their Avarage is "+str(avarage))
