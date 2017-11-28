8b7207e0702a4a5acf81721dde099a91f969898c
DonBattery <mikloslorinczi.it@gmail.com>
2017-11-28 17:01:47 +0100

    added append_a, fixed matrix.py

---
+++ week-02/day-2/append_a.py
@@ -0,0 +1,9 @@
+animals = ["kuty", "macsk", "cic"]
+
+print('Before :' + str(animals))
+
+for i in range(int(len((animals)))):
+    animals[i] = animals[i] + 'a'
+
+print('After : ' + str(animals))
+
--- week-02/day-2/matrix.py
+++ week-02/day-2/matrix.py
@@ -1,15 +1,19 @@
 a = []
 b = []
-size = 4
+size = 9
 
 for i in range(size):
     b = []
     for j in range(size):
         if i == j: 
-            b.append(' ')
+            b.append('o')
         else:
             b.append('#')
     a.append(b)
 
+for row in a:
+    print(" ".join('{0:2}'.format(i or " ") for i in row))
+
+'''   
 for i in range(size):
-    print(a[i])
+    print(a[i]) '''
