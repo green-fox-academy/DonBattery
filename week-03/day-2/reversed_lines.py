4bc4646581e4c8eebf4fc8e8b9872184dbfea6c4
DonBattery <mikloslorinczi.it@gmail.com>
2017-12-05 13:46:47 +0100

    reversed_lines.py completed

--- week-03/day-2/reversed_lines.py
+++ week-03/day-2/reversed_lines.py
@@ -1,3 +1,20 @@
 # Create a method that decrypts reversed-lines.txt
-def decrypt(file_name):
-    pass
+
+def decrypt(file_name):    
+    try:
+        with open(file_name, 'r') as efile:
+            lines = efile.readlines()   
+    except IOError:
+        print('Unable to read file')
+    decripted = []
+    for line in lines:
+        decripted.append(line[::-1])
+    try:
+        with open('decripted.txt', 'w') as dfile:
+            dfile.writelines(decripted)
+    except IOError:
+        print('Unable to write to file')
+
+decrypt(r'''C:\Users\Miki\greenfox\DonBattery\week-03\day-2\reversed-lines.txt''')
+
+
