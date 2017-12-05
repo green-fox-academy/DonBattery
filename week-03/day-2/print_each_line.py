d516e7a9020447994a09e166c78c52060a6901d4
DonBattery <mikloslorinczi.it@gmail.com>
2017-12-05 11:53:47 +0100

    print_each_line.py completed

--- week-03/day-2/print_each_line.py
+++ week-03/day-2/print_each_line.py
@@ -2,3 +2,17 @@
 # each of lines form the file.
 # If the program is unable to read the file (for example it does not exists),
 # then it should print an error message like: "Unable to read file: my-file.txt"
+
+opened = False
+
+try:
+    f = open('my-file.txt', 'r')
+    opened = True
+except IOError:
+    print('Unable to read file: my-file.txt')
+
+if opened:
+    lines = f.readlines()
+    for line in lines:
+        print(line)
+    f.close()
