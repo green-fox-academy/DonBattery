62f36e8c686c1f9f7d65700e5dc361d05ac7a14b
DonBattery <mikloslorinczi.it@gmail.com>
2017-12-05 15:26:53 +0100

    encoded_lines.py working

--- week-03/day-2/encoded_lines.py
+++ week-03/day-2/encoded_lines.py
@@ -1,3 +1,31 @@
 # Create a method that decrypts encoded-lines.txt
-def decrypt(file_name):
-    pass
+
+def decrypt(file_name):    
+    try:
+        with open(file_name, 'r') as efile:
+            lines = efile.readlines()   
+    except IOError:
+        print('Unable to read file')
+    decripted = []
+    dline = ''
+    dchar = ''
+    print(lines)
+    for line in range(len(lines)):
+        for i in range(len(lines[line])):
+            dchar = lines[line][i]
+            if dchar.isalpha() or dchar in ['[','{','(','"']: 
+                dchar = chr(ord(dchar) - 1)
+            dline += dchar
+        decripted.append(dline)
+        dline = ''
+    print(decripted)
+    try:
+        with open('decripted3.txt', 'w') as dfile:
+            dfile.writelines(decripted)
+    except IOError:
+        print('Unable to write to file')
+
+decrypt(r'''C:\Users\Miki\greenfox\DonBattery\week-03\day-2\encoded-lines.txt''')
+
+
+
