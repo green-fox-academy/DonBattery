1380209b97822a925abfbad251af035f71fdfb12
DonBattery <mikloslorinczi.it@gmail.com>
2017-12-05 11:59:38 +0100

    count_lines.py completed

--- week-03/day-2/count_lines.py
+++ week-03/day-2/count_lines.py
@@ -2,3 +2,14 @@
 # then returns the number of lines the file contains.
 # It should return zero if it can't open the file, and
 # should not raise any error.
+
+def linecounter(FFilename):
+    try:
+        with open(FFilename) as f:
+            return len(f.readlines())
+    except IOError:
+        return 0
+
+print(linecounter('my-file.txt'))
+
+print(linecounter('sajt'))
