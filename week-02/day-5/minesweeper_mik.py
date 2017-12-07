195b51a97cec3ff3d735ebc5e6f23f3e0b8d7f6b
DonBattery <mikloslorinczi.it@gmail.com>
2017-12-07 12:41:19 +0100

    added colors, minor cosmetics, screenshot

--- week-02/day-5/minesweeper_mik.py
+++ week-02/day-5/minesweeper_mik.py
@@ -1,5 +1,8 @@
 from os import system # OS for clear the screen
 import random # import this to get random numbers
+from termcolor import cprint # this needs for the colors in the terminal (pip install termcolor)
+import sys # we need this for the character codec setup
+import codecs # also this is needed to set up the character codec
 
 # The initial size of the minefield (it is allways square)
 SIZE = 9
@@ -9,6 +12,9 @@ MINES = 9
 
 # Allign the minefield a bit to the right
 Indention = 20
+
+# Assign colors to "numbers" (mine indexes on the field)
+colors = ['white', 'blue', 'magenta', 'red', 'cyan', 'red', 'red', 'red', 'red']
 
 # Initialize the minefield as a matrix (list within list) of dictionaries containing the attributes of the individual fields
 def init_map(size):
@@ -38,36 +44,42 @@ def init_numbers(grid):
 def clear_screen():
     system('clear')
 
-# Drow out the grid
+# Draw out the grid
 def draw(grid):
     clear_screen()
     print('\n')
-    print('¸,ø¤°º¤ø,¸¸,ø¤º°`°º¤ø,¸- =M i n e S w e e p e R= -¸,ø¤º°`°º¤ø,¸,ø¤°º¤ø,¸¸')
-    print('        ¸,ø¤°º¤ø,¸. o O', end ='')
+    cprint('. o O o . o O o . o O o--=M i n e S w e e p e R=--o O o . o O o . o O o .', 'yellow')
+    cprint('_    _   __   ___ ____|', 'yellow', end = '')
     for i in range(1, SIZE + 1):
         if i < 10:
             symbol = ' ' + str(i) + ' '
         else:
             symbol = str(i) + ' ' 
-        print(symbol, end = '')
-    print('O o .¸,ø¤°º¤ø,¸ ¸')   
+        cprint(symbol, 'green', end = '')
+    cprint('|____ ___  __   _     _', 'yellow')   
     for y_cord in range(0, SIZE):
         if y_cord + 1 < 10:
             symbol = ' ' + str(y_cord + 1) + ' '
         else:
             symbol = str(y_cord + 1) + ' '
-        print(Indention * ' ' + symbol, end ='')
+        cprint(Indention * ' ' + symbol, 'green', end ='')
         for x_cord in range(0, SIZE):
             if grid[x_cord][y_cord]['isMine'] and grid[x_cord][y_cord]['isChecked']:
                 symbol = 'X'
+                color = 'red'
             elif grid[x_cord][y_cord]['isChecked']:
                 if grid[x_cord][y_cord]['number'] > 0:
+                    color = colors[grid[x_cord][y_cord]['number']]
                     symbol = str(grid[x_cord][y_cord]['number'])
                 else:
+                    color = colors[0]
                     symbol = ' '
             else:
+                color = 'white'
                 symbol = '?'
-            print('[' + symbol + ']', end='')
+            print('[', end = '')
+            cprint(symbol, color, end = '')
+            print(']', end='')
         print('\n')
 
 # Recursive algorithm to discover numbers around a given field (if it finds a zero it calls itself on that position)
@@ -89,17 +101,20 @@ def check(grid, x_cord, y_cord):
         discover(grid, x_cord, y_cord)
     return False
 
-# Get player input
+# Get player input (halts program on userinput Q or q)
 def get_xy_cord():
     correct = False
     while not correct:
-        Sinput = input(' ' * Indention + 'Which field to check ? (X Y) 1 - ' + str(SIZE) + ' : ')
-        if len(Sinput) >=3 and Sinput.count(' ') == 1 and Sinput.find(' ') != 0 and Sinput.find(' ') != len(Sinput) - 1:
-            x_cord, y_cord = Sinput.split()
-            if x_cord.isnumeric() and y_cord.isnumeric():
-                x_cord, y_cord = int(x_cord), int(y_cord)
-                if 0 < x_cord <= SIZE and 0 < y_cord <= SIZE:
-                    correct = True
+        Sinput = input(' ' * Indention + ' Field to check ? (X Y) 1 - ' + str(SIZE) + ' : ')
+        if (len(Sinput) >=3 and Sinput.count(' ') == 1 and Sinput.find(' ') != 0 and Sinput.find(' ') != len(Sinput) - 1) or (Sinput in ['q', 'Q']):
+            if Sinput not in ['q', 'Q']:
+                x_cord, y_cord = Sinput.split()
+                if x_cord.isnumeric() and y_cord.isnumeric():
+                    x_cord, y_cord = int(x_cord), int(y_cord)
+                    if 0 < x_cord <= SIZE and 0 < y_cord <= SIZE:
+                        correct = True
+            else:
+                quit() # I know, I know....
     return x_cord - 1, y_cord - 1
 
 # Check if the player has won the game
@@ -111,8 +126,16 @@ def won_situation(grid):
                 already_checked += 1
     return already_checked == SIZE ** 2 - MINES
 
+# Setup the character encode on the terminal
+def set_encode():
+    if sys.stdout.encoding != 'cp850':
+        sys.stdout = codecs.getwriter('cp850')(sys.stdout.buffer, 'strict')
+    if sys.stderr.encoding != 'cp850':
+        sys.stderr = codecs.getwriter('cp850')(sys.stderr.buffer, 'strict')
+
 # Initialize everything and enter into the main game loop (which end on won or lost condition)
-def start_game(): 
+def start_game():
+    set_encode()
     grid = init_map(SIZE)
     init_mines(grid, MINES)
     init_numbers(grid)
@@ -125,8 +148,8 @@ def start_game():
         won = won_situation(grid)
         draw(grid)
     if won:
-        print('You are a winner!')
+        cprint(' ' * Indention + ' You are a winner!', 'green')
     else:
-        print('You just lost The Game!')
+        cprint(' ' * Indention + ' You just lost The Game!', 'red')
 
 start_game()
