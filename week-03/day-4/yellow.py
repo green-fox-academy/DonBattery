4e1ab422fe638cc6d49e8bf466516a0f4bd929d7
DonBattery <mikloslorinczi.it@gmail.com>
2017-12-07 14:35:22 +0100

    yellow added

---
+++ week-03/day-4/yellow.py
@@ -0,0 +1,30 @@
+# Sárga Rekt
+
+from tkinter import *
+
+root = Tk()
+
+canvas = Canvas(root, width='300', height='300')
+canvas.pack()
+
+maxx = int(canvas['width'])
+maxy = int(canvas['height'])
+
+def rekt(topx, topy, botx, boty):
+    if botx - topx <= 12:
+        return
+    xdif = botx - topx
+    ydif = boty - topy
+    canvas.create_rectangle(topx, topy, botx, boty, outline='black', fill='yellow')
+    canvas.create_line(topx + xdif // 3, topy, topx + xdif // 3, boty, fill = 'black', width = 1)
+    canvas.create_line(topx + (xdif // 3) * 2, topy, topx + (xdif // 3) * 2, boty, fill = 'black', width = 1)
+    canvas.create_line(topx, topy + ydif // 3, botx, topy + ydif // 3, fill = 'black', width = 1)
+    canvas.create_line(topx, topy + (ydif // 3) * 2, botx, topy + (ydif // 3) * 2, fill = 'black', width = 1)
+    rekt(topx + xdif // 3, topy, topx + (xdif // 3) * 2, topy + ydif // 3) # top
+    rekt(topx, topy + ydif // 3, topx + (xdif // 3), topy + (ydif // 3) * 2) # left
+    rekt(topx + (xdif // 3) * 2, topy + ydif // 3, botx, topy + (ydif // 3) * 2) # right
+    rekt(topx + xdif // 3, topy + (ydif // 3) * 2, topx + (xdif // 3) * 2, boty) # bot
+
+rekt(0, 0, maxx, maxy)
+
+root.mainloop()
