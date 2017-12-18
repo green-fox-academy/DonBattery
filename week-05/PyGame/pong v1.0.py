0ff2ac729b3e1f69778888669d4bd12d6d5be878
DonBattery <mikloslorinczi.it@gmail.com>
2017-12-19 00:16:43 +0100

    PYGame test and RPGame skeleton added pls do not review

---
+++ week-05/PyGame/pong v1.0.py
@@ -0,0 +1,134 @@
+#!/usr/bin/env python
+#
+#       This program is free software; you can redistribute it and/or modify
+#       it under the terms of the GNU General Public License as published by
+#       the Free Software Foundation; either version 2 of the License, or
+#       (at your option) any later version.
+#       
+#       This program is distributed in the hope that it will be useful,
+#       but WITHOUT ANY WARRANTY; without even the implied warranty of
+#       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#       GNU General Public License for more details.
+#
+#		It's my first actual game-making attempt. I know code could be much better 
+#		with classes or defs but I tried to make it short and understandable with very 
+#		little knowledge of python and pygame(I'm one of them). Enjoy.
+
+import pygame
+from pygame.locals import *
+from sys import exit
+import random
+
+pygame.init()
+
+pygame.display.set_caption("RPGame")
+
+screen = pygame.display.set_mode((640, 480), pygame.FULLSCREEN)
+
+
+#The background
+back = pygame.Surface((640, 480)) 
+background = back.convert() # Convert to have the same pixel format as the display Surface.
+background.fill((200, 200, 20))
+
+#Alap kocka
+bar = pygame.image.load('badcat2.png')
+
+#Kigirikonvertálj
+bar1 = bar.convert()
+
+
+bar2 = bar.convert()
+
+
+# some definitions
+bar1_x, bar2_x = 10, 300
+bar1_y, bar2_y = 15, 15
+
+#clock and font objects
+clock = pygame.time.Clock()
+font = pygame.font.SysFont("calibri", 18)
+
+# main loop
+while True:
+    
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            exit()
+        if event.type == KEYDOWN:
+            if event.key == K_ESCAPE:
+                exit()
+
+    score1 = font.render("sajtosfasz", True,(255,255,255))
+
+
+
+    screen.blit(background,(0,0))
+
+    screen.blit(bar1,(bar1_x,bar1_y))
+
+    screen.blit(bar2,(bar2_x,bar2_y))
+
+    screen.blit(score1,(150,0))
+        
+    pygame.display.update()
+
+''' 
+    bar1_y += bar1_move
+    
+# movement of circle
+    time_passed = clock.tick(30)
+    time_sec = time_passed / 1000.0
+    
+    circle_x += speed_x * time_sec
+    circle_y += speed_y * time_sec
+    ai_speed = speed_circ * time_sec
+#AI of the computer.
+    if circle_x >= 305.:
+        if not bar2_y == circle_y + 7.5:
+            if bar2_y < circle_y + 7.5:
+                bar2_y += ai_speed
+            if  bar2_y > circle_y - 42.5:
+                bar2_y -= ai_speed
+        else:
+            bar2_y == circle_y + 7.5
+    
+    if bar1_y >= 420.: bar1_y = 420.
+    elif bar1_y <= 10. : bar1_y = 10.
+    if bar2_y >= 420.: bar2_y = 420.
+    elif bar2_y <= 10.: bar2_y = 10.
+#since i don't know anything about collision, ball hitting bars goes like this.
+    if circle_x <= bar1_x + 10.:
+        if circle_y >= bar1_y - 7.5 and circle_y <= bar1_y + 42.5:
+            circle_x = 20.
+            speed_x = -speed_x
+    if circle_x >= bar2_x - 15.:
+        if circle_y >= bar2_y - 7.5 and circle_y <= bar2_y + 42.5:
+            circle_x = 605.
+            speed_x = -speed_x
+    if circle_x < 5.:
+        bar2_score += 1
+        circle_x, circle_y = 640., 232.5
+        bar1_y,bar_2_y = 215., 215.
+    elif circle_x > 620.:
+        bar1_score += 1
+        circle_x, circle_y = 307.5, 232.5
+        bar1_y, bar2_y = 215., 215.
+    if circle_y <= 10.:
+        speed_y = -speed_y
+        circle_y = 10.
+    elif circle_y >= 457.5:
+        speed_y = -speed_y
+        circle_y = 457.5
+ '''
+
+'''      if event.type == KEYDOWN:
+            if event.key == K_UP:
+                bar1_move = -ai_speed
+            elif event.key == K_DOWN:
+                bar1_move = ai_speed
+        elif event.type == KEYUP:
+            if event.key == K_UP:
+                bar1_move = 0.
+            elif event.key == K_DOWN:
+                bar1_move = 0. '''
