2992d468a604f02f32537e2118afb9bd54bc3592
DonBattery <mikloslorinczi.it@gmail.com>
2017-12-20 23:53:36 +0100

    started developing the mapper

--- week-05/PyGame/MappR.py
+++ week-05/PyGame/MappR.py
@@ -1,9 +1,19 @@
 # Mapper
 
-class Tile():
-    def __init__(self):
-        pass
+import pygame
+from pygame.locals import *
+import ImagR
+import FilR
+
+class Tile_set():
+    def __init__(self, filepath, file, size):
+        self.image = ImagR.load_image(filepath, file)
+        self.size = size
 
 class Map():
-    def __init__(self):
-        pass
+    def __init__(self, filepath, file):
+        self.file_con = FilR.File_Controller(filepath, file)
+        self.file_con.test_file()
+        self.data = self.file_con.get_lines()
+        self.info = self.data[0].split(';')
+        self.tilemap = self.data[1].split(';')
