50ab5b0a45276d8ea5ada312374e973a80394d6a
DonBattery <mikloslorinczi.it@gmail.com>
2018-01-11 12:03:03 +0100

    imports and main class declared

---
+++ week-07/day-4/gallery.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+import pygame
+from pygame.locals import *
+import PIL
+import os.path
+
+class Gallery_app():
+
+    def __init__(self):
+        pass
+    
+    def go(self):
+        pass
+
+gallery = Gallery_app()
+
+if __name__ == '__main__':
+    gallery.go()
