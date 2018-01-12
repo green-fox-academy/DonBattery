4a03bea912e06bd5df335ff02446af9925f26ecc
DonBattery <mikloslorinczi.it@gmail.com>
2018-01-12 14:18:55 +0100

    Erik removed

--- week-03/day-5/randomstudents.py
+++ week-03/day-5/randomstudents.py
@@ -1,6 +1,6 @@
 from random import shuffle
 
-Students = ['Erik','Ádám', 'Alexandra', 'Andris', 'Béla', 'Fruzsi', 'Kinga', 'MG', 'Miki', 'Tamás', 'Bálint', 'Bandi', 'Patrik', 'Emese', 'Laci', 'Nándi', 'Rudi', 'Hédi']
+Students = ['Ádám', 'Alexandra', 'Andris', 'Béla', 'Fruzsi', 'Kinga', 'MG', 'Miki', 'Tamás', 'Bálint', 'Bandi', 'Patrik', 'Emese', 'Laci', 'Nándi', 'Rudi', 'Hédi']
 shuffle(Students)
 print(Students) 
    
