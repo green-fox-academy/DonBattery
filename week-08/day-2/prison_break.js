2922d55f523baffa7d489be3b27a01b9411c40fe
DonBattery <mikloslorinczi.it@gmail.com>
2018-01-16 12:39:47 +0100

    prison_brake.js added

---
+++ week-08/day-2/prison_break.js
@@ -0,0 +1,33 @@
+// Create a prison function, that has your name as a private fugutive variable
+// The function should return an object that has two methods:
+//  - visit() // logs "[yourname] is visited [x] time(s)" to the console.
+//    - the [x] times displayes the numbers the function is called
+//    - If the fugitive variable is empty, then display "Nobody is here anymore"
+//  - escape() // logs "BREAKING NEWS, [yourname] escaped the prison" to the console.
+//    - it should empties the fugitive variable
+
+function prison(name){
+    let fugitive = name;
+    let visited = 0;
+    return {
+        visit : function() {
+            if (fugitive.length > 0) {
+                console.log(fugitive + ' is visited ' + ++visited + ' time(s)');
+            }
+            else {
+                console.log('Nobody is here anymore');
+            }
+        },
+        escape : function() {
+            console.warn('BREAKING NEWS, ' + fugitive + ' escaped the prison!');
+            fugitive = '';
+        }      
+    }
+}
+
+const alcatraz = prison('Sad Panda');
+
+alcatraz.visit() // Sad Panda is visited 1 time(s)
+alcatraz.visit() // Sad Panda is visited 2 time(s)
+alcatraz.escape() // BREAKING NEWS, Sad Panda escaped the prison
+alcatraz.visit() // Nobody is here anymore
